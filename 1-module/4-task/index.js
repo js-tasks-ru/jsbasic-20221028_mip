0ca0a063b8c215b2546a1e4fe12efa0e7d39f37b
Ilya <morozovip@mail.ru>
2022-11-01 18:46:20 +0300

    Done task 4 module 1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  if (str.toLowerCase().includes('1xbet')) {
+    return true
+  }
+  else if (str.toLowerCase().includes('xxx')) {
+    return true
+  } else {
+    return false
+  }
 }
